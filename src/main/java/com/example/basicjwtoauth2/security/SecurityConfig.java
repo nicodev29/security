86c2297389d604c5eac57f6fe0a8c27a6d70f43b
Nicolas <nicogarcia1229@gmail.com>
2024-01-07 02:40:42 -0300

    adding_custom_users

--- src/main/java/com/example/basicjwtoauth2/security/SecurityConfig.java
+++ src/main/java/com/example/basicjwtoauth2/security/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,4 +21,22 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+
+        var admin = User.withUsername("admin")
+                .password("admin")
+                .authorities("ADMIN")
+                .build();
+
+        var user = User.withUsername("user")
+                .password("user")
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
 }
